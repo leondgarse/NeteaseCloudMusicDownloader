4c42f3237b24b932465fe00d687aed372d15d759
leondgarse <leondgarse@gmail.com>
2021-03-28 16:08:29 +0800

    Reformat encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -7,16 +7,22 @@ import binascii
 from Crypto.Cipher import AES
 
 # Encrypt key
-modulus = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
-nonce = b"0CoJUm6Qyw8W8jud"
-pub_key = "010001"
+MODULUS = (
+    "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7"
+    "b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280"
+    "104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932"
+    "575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b"
+    "3ece0462db0a22b8e7"
+)
+PUBKEY = "010001"
+NONCE = b"0CoJUm6Qyw8W8jud"
 
 
 def encrypted_request(text):
     text = json.dumps(text)
     sec_key = create_secret_key(16)
-    enc_text = aes_encrypt(aes_encrypt(text, nonce), sec_key)
-    enc_sec_key = rsa_encrypt(sec_key, pub_key, modulus)
+    enc_text = aes_encrypt(aes_encrypt(text, NONCE), sec_key)
+    enc_sec_key = rsa_encrypt(sec_key, PUBKEY, MODULUS)
     data = {"params": enc_text, "encSecKey": enc_sec_key}
     return data
 
